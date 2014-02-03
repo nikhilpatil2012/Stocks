3d47e65f3448f7e928d053979c025a144d887c57
Nikhil <nikhil@Nikhils-MacBook-Air.local>
2014-02-03 11:15:36 +0530

    Initial Commit

---
+++ Stocks/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Stocks
+//
+//  Created by Nikhil on 2/3/14.
+//  Copyright (c) 2014 Nikhil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
